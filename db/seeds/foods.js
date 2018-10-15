c25989bc97d19effd64e3208d487657f1d9d5a08
jtrtj <jt.r@live.com>
2018-10-15 17:26:11 -0600

    made seed data for foods and meals

---
+++ db/seeds/foods.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    name: 'Strawberries',
+    calories: 120
+  },
+  {
+    name: 'Blackberries',
+    calories: 110
+  },
+  {
+    name: 'Blueberries',
+    calories: 140
+  },
+  {
+    name: 'Raspberries',
+    calories: 200
+  },
+  {
+    name: 'Boysenberries',
+    calories: 150
+  },
+  {
+    name: 'Gooseberries',
+    calories: 220
+  },
+  {
+    name: 'Poisonberries',
+    calories: 420
+  },
+  {
+    name: 'Açaí Berries',
+    calories: 90
+  },
+  {
+    name: 'Mulberries',
+    calories: 100
+  },
+  {
+    name: 'Astroberries',
+    calories: 777
+  }
+]
