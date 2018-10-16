023d71047dd5e7819bbff1151084030616db08ca
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-10-16 02:23:30 -0600

    fetch meal and show meal now two separate methods

--- routes/api/meals.js
+++ routes/api/meals.js
@@ -11,15 +11,6 @@ const query_all = `SELECT meals.id, meals.name, array_to_json
        JOIN meal_foods ON meals.id = meal_foods.meal_id
        JOIN foods ON meal_foods.food_id = foods.id
        GROUP BY meals.id`;
-let meal_id;
-const query_single = `SELECT meals.id, meals.name, array_to_json
-       (array_agg(json_build_object('id', foods.id, 'name', foods.name, 'calories', foods.calories)))
-       AS foods
-       FROM meals
-       WHERE (id = ?, ${meal_id})
-       JOIN meal_foods ON meals.id = meal_foods.meal_id
-       JOIN foods ON meal_foods.food_id = foods.id
-       GROUP BY meals.id`;
 
 router.get("/", (req, res) => {
   return database
@@ -32,12 +23,21 @@ router.get("/", (req, res) => {
     });
 });
 
+const getSingle = meal_id => {
+  return database.raw(`SELECT meals.id, meals.name, array_to_json
+             (array_agg(json_build_object('id', foods.id, 'name', foods.name, 'calories', foods.calories)))
+             AS foods
+             FROM meals
+             JOIN meal_foods ON meals.id = meal_foods.meal_id
+             JOIN foods ON meal_foods.food_id = foods.id
+             WHERE meals.id = ${meal_id}
+             GROUP BY meals.id`);
+};
+
 router.get("/:meal_id/foods", (req, res) => {
-  meal_id = req.params.meal_id;
-  return database
-    .raw(query_single)
-    .then(meal => {
-      res.status(200).json(meal.rows);
+  found_meal = getSingle(req.params.meal_id)
+    .then(found_meal => {
+      res.status(200).json(found_meal.rows);
     })
     .catch(error => {
       res.status(500).json({ error });
