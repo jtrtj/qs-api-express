4a67bc8878a420aade32ee428a5819bbdd48572f
jtrtj <jt.r@live.com>
2018-10-15 21:57:04 -0600

    pass client routes test for root

--- tests/foodRoutes.spec.js
+++ tests/foodRoutes.spec.js
@@ -1,9 +1,22 @@
 const chai = require("chai");
 const should = chai.should();
 const chaiHttp = require("chai-http");
+const server = require("../index");
 
 chai.use(chaiHttp);
 
-describe("Client Routes", () => {});
+describe("Client Routes", () => {
+  it("should return the homepage with text", done => {
+    chai
+      .request(server)
+      .get("/")
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.be.html;
+        response.res.text.should.equal("Welcome to the Quantified Self API");
+        done();
+      });
+  });
+});
 
 describe("API Routes", () => {});
