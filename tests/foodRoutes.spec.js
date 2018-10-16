8238b6433a6b2b3863aa180668b20e43de9558c9
jtrtj <jt.r@live.com>
2018-10-15 21:46:28 -0600

    add mocha/chai/chai-http and start foods routes test

---
+++ tests/foodRoutes.spec.js
@@ -0,0 +1,9 @@
+const chai = require("chai");
+const should = chai.should();
+const chaiHttp = require("chai-http");
+
+chai.use(chaiHttp);
+
+describe("Client Routes", () => {});
+
+describe("API Routes", () => {});
