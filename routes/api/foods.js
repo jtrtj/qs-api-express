4516d302e2d1048c07a5b1f9ea9c2a35e837acc9
jtrtj <jt.r@live.com>
2018-10-15 20:00:38 -0600

    add working routes for foods index and foods show install body-parser

--- routes/api/foods.js
+++ routes/api/foods.js
@@ -3,7 +3,19 @@ const router = express.Router();
 const db = require("../../db");
 
 router.get("/", function(req, res) {
-  db.select().from("");
+  db.select()
+    .from("foods")
+    .then(function(data) {
+      res.send(data);
+    });
+});
+
+router.get("/:id", function(req, res) {
+  db.whereIn("id", [req.params.id])
+    .from("foods")
+    .then(function(data) {
+      res.send(data);
+    });
 });
 
 module.exports = router;
